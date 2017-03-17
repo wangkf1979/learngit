6ae8b87ef77beb69f0e38f0152ec6414cd8a6d11
kefeng.wang <kefeng.wang@ca.com>
2017-03-17 13:06:15 +0800

    US38

---
+++ rallydemo/src/com/ca/DemoClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ca;
+
+/**
+ * @author wangkf1979
+ * This is a comments!
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
